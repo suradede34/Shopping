ff78510d3b380ad5eeee7eaf7c982c63bd1487ba
suradede34 <suradedeyy@gmail.com>
2024-11-28 19:24:40 +0300

    Add files via upload

---
+++ assets/js/shopping.js
@@ -0,0 +1,556 @@
+products = [
+  {
+  "id": 1,
+  "title": "Essence Mascara Lash Princess",
+  "description": "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects. Achieve dramatic lashes with this long-lasting and cruelty-free formula.",
+  "category": "beauty",
+  "price": 9.99,
+  "stock": 5,
+  "tags": [
+  "beauty",
+  "mascara"
+  ],
+  "brand": "Essence",
+  "images": [
+  "https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/1.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png"
+  },
+  {
+  "id": 2,
+  "title": "Eyeshadow Palette with Mirror",
+  "description": "The Eyeshadow Palette with Mirror offers a versatile range of eyeshadow shades for creating stunning eye looks. With a built-in mirror, it's convenient for on-the-go makeup application.",
+  "category": "beauty",
+  "price": 19.99,
+  "stock": 44,
+  "tags": [
+  "beauty",
+  "eyeshadow"
+  ],
+  "brand": "Glamour Beauty",
+  "images": [
+  "https://cdn.dummyjson.com/products/images/beauty/Eyeshadow%20Palette%20with%20Mirror/1.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/beauty/Eyeshadow%20Palette%20with%20Mirror/thumbnail.png"
+  },
+  {
+  "id": 3,
+  "title": "Powder Canister",
+  "description": "The Powder Canister is a finely milled setting powder designed to set makeup and control shine. With a lightweight and translucent formula, it provides a smooth and matte finish.",
+  "category": "beauty",
+  "price": 14.99,
+  "stock": 59,
+  "tags": [
+  "beauty",
+  "face powder"
+  ],
+  "brand": "Velvet Touch",
+  "images": [
+  "https://cdn.dummyjson.com/products/images/beauty/Powder%20Canister/1.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/beauty/Powder%20Canister/thumbnail.png"
+  },
+  {
+  "id": 4,
+  "title": "Red Lipstick",
+  "description": "The Red Lipstick is a classic and bold choice for adding a pop of color to your lips. With a creamy and pigmented formula, it provides a vibrant and long-lasting finish.",
+  "category": "beauty",
+  "price": 12.99,
+  "stock": 68,
+  "tags": [
+  "beauty",
+  "lipstick"
+  ],
+  "brand": "Chic Cosmetics",
+  "images": [
+  "https://cdn.dummyjson.com/products/images/beauty/Red%20Lipstick/1.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/beauty/Red%20Lipstick/thumbnail.png"
+  },
+  {
+  "id": 5,
+  "title": "Red Nail Polish",
+  "description": "The Red Nail Polish offers a rich and glossy red hue for vibrant and polished nails. With a quick-drying formula, it provides a salon-quality finish at home.",
+  "category": "beauty",
+  "price": 8.99,
+  "stock": 71,
+  "tags": [
+  "beauty",
+  "nail polish"
+  ],
+  "brand": "Nail Couture",
+  "images": [
+  "https://cdn.dummyjson.com/products/images/beauty/Red%20Nail%20Polish/1.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/beauty/Red%20Nail%20Polish/thumbnail.png"
+  },
+  {
+  "id": 6,
+  "title": "Calvin Klein CK One",
+  "description": "CK One by Calvin Klein is a classic unisex fragrance, known for its fresh and clean scent. It's a versatile fragrance suitable for everyday wear.",
+  "category": "fragrances",
+  "price": 49.99,
+  "stock": 17,
+  "tags": [
+  "fragrances",
+  "perfumes"
+  ],
+  "brand": "Calvin Klein",
+  "images": [
+  "https://cdn.dummyjson.com/products/images/fragrances/Calvin%20Klein%20CK%20One/1.png",
+  "https://cdn.dummyjson.com/products/images/fragrances/Calvin%20Klein%20CK%20One/2.png",
+  "https://cdn.dummyjson.com/products/images/fragrances/Calvin%20Klein%20CK%20One/3.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/fragrances/Calvin%20Klein%20CK%20One/thumbnail.png"
+  },
+  {
+  "id": 7,
+  "title": "Chanel Coco Noir Eau De",
+  "description": "Coco Noir by Chanel is an elegant and mysterious fragrance, featuring notes of grapefruit, rose, and sandalwood. Perfect for evening occasions.",
+  "category": "fragrances",
+  "price": 129.99,
+  "stock": 41,
+  "tags": [
+  "fragrances",
+  "perfumes"
+  ],
+  "brand": "Chanel",
+  "images": [
+  "https://cdn.dummyjson.com/products/images/fragrances/Chanel%20Coco%20Noir%20Eau%20De/1.png",
+  "https://cdn.dummyjson.com/products/images/fragrances/Chanel%20Coco%20Noir%20Eau%20De/2.png",
+  "https://cdn.dummyjson.com/products/images/fragrances/Chanel%20Coco%20Noir%20Eau%20De/3.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/fragrances/Chanel%20Coco%20Noir%20Eau%20De/thumbnail.png"
+  },
+  {
+  "id": 8,
+  "title": "Dior J'adore",
+  "description": "J'adore by Dior is a luxurious and floral fragrance, known for its blend of ylang-ylang, rose, and jasmine. It embodies femininity and sophistication.",
+  "category": "fragrances",
+  "price": 89.99,
+  "stock": 91,
+  "tags": [
+  "fragrances",
+  "perfumes"
+  ],
+  "brand": "Dior",
+  "images": [
+  "https://cdn.dummyjson.com/products/images/fragrances/Dior%20J'adore/1.png",
+  "https://cdn.dummyjson.com/products/images/fragrances/Dior%20J'adore/2.png",
+  "https://cdn.dummyjson.com/products/images/fragrances/Dior%20J'adore/3.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/fragrances/Dior%20J'adore/thumbnail.png"
+  },
+  {
+  "id": 9,
+  "title": "Dolce Shine Eau de",
+  "description": "Dolce Shine by Dolce & Gabbana is a vibrant and fruity fragrance, featuring notes of mango, jasmine, and blonde woods. It's a joyful and youthful scent.",
+  "category": "fragrances",
+  "price": 69.99,
+  "stock": 3,
+  "tags": [
+  "fragrances",
+  "perfumes"
+  ],
+  "brand": "Dolce & Gabbana",
+  "images": [
+  "https://cdn.dummyjson.com/products/images/fragrances/Dolce%20Shine%20Eau%20de/1.png",
+  "https://cdn.dummyjson.com/products/images/fragrances/Dolce%20Shine%20Eau%20de/2.png",
+  "https://cdn.dummyjson.com/products/images/fragrances/Dolce%20Shine%20Eau%20de/3.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/fragrances/Dolce%20Shine%20Eau%20de/thumbnail.png"
+  },
+  {
+  "id": 10,
+  "title": "Gucci Bloom Eau de",
+  "description": "Gucci Bloom by Gucci is a floral and captivating fragrance, with notes of tuberose, jasmine, and Rangoon creeper. It's a modern and romantic scent.",
+  "category": "fragrances",
+  "price": 79.99,
+  "stock": 93,
+  "tags": [
+  "fragrances",
+  "perfumes"
+  ],
+  "brand": "Gucci",
+  "images": [
+  "https://cdn.dummyjson.com/products/images/fragrances/Gucci%20Bloom%20Eau%20de/1.png",
+  "https://cdn.dummyjson.com/products/images/fragrances/Gucci%20Bloom%20Eau%20de/2.png",
+  "https://cdn.dummyjson.com/products/images/fragrances/Gucci%20Bloom%20Eau%20de/3.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/fragrances/Gucci%20Bloom%20Eau%20de/thumbnail.png"
+  },
+  {
+  "id": 11,
+  "title": "Annibale Colombo Bed",
+  "description": "The Annibale Colombo Bed is a luxurious and elegant bed frame, crafted with high-quality materials for a comfortable and stylish bedroom.",
+  "category": "furniture",
+  "price": 1899.99,
+  "stock": 47,
+  "tags": [
+  "furniture",
+  "beds"
+  ],
+  "brand": "Annibale Colombo",
+  "images": [
+  "https://cdn.dummyjson.com/products/images/furniture/Annibale%20Colombo%20Bed/1.png",
+  "https://cdn.dummyjson.com/products/images/furniture/Annibale%20Colombo%20Bed/2.png",
+  "https://cdn.dummyjson.com/products/images/furniture/Annibale%20Colombo%20Bed/3.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/furniture/Annibale%20Colombo%20Bed/thumbnail.png"
+  },
+  {
+  "id": 12,
+  "title": "Annibale Colombo Sofa",
+  "description": "The Annibale Colombo Sofa is a sophisticated and comfortable seating option, featuring exquisite design and premium upholstery for your living room.",
+  "category": "furniture",
+  "price": 2499.99,
+  "stock": 16,
+  "tags": [
+  "furniture",
+  "sofas"
+  ],
+  "brand": "Annibale Colombo",
+  "images": [
+  "https://cdn.dummyjson.com/products/images/furniture/Annibale%20Colombo%20Sofa/1.png",
+  "https://cdn.dummyjson.com/products/images/furniture/Annibale%20Colombo%20Sofa/1.png",
+  "https://cdn.dummyjson.com/products/images/furniture/Annibale%20Colombo%20Sofa/2.png",
+  "https://cdn.dummyjson.com/products/images/furniture/Annibale%20Colombo%20Sofa/3.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/furniture/Annibale%20Colombo%20Sofa/thumbnail.png"
+  },
+  {
+  "id": 13,
+  "title": "Bedside Table African Cherry",
+  "description": "The Bedside Table in African Cherry is a stylish and functional addition to your bedroom, providing convenient storage space and a touch of elegance.",
+  "category": "furniture",
+  "price": 299.99,
+  "stock": 16,
+  "tags": [
+  "furniture",
+  "bedside tables"
+  ],
+  "brand": "Furniture Co.",
+  "images": [
+  "https://cdn.dummyjson.com/products/images/furniture/Bedside%20Table%20African%20Cherry/1.png",
+  "https://cdn.dummyjson.com/products/images/furniture/Bedside%20Table%20African%20Cherry/2.png",
+  "https://cdn.dummyjson.com/products/images/furniture/Bedside%20Table%20African%20Cherry/3.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/furniture/Bedside%20Table%20African%20Cherry/thumbnail.png"
+  },
+  {
+  "id": 14,
+  "title": "Knoll Saarinen Executive Conference Chair",
+  "description": "The Knoll Saarinen Executive Conference Chair is a modern and ergonomic chair, perfect for your office or conference room with its timeless design.",
+  "category": "furniture",
+  "price": 499.99,
+  "stock": 47,
+  "tags": [
+  "furniture",
+  "office chairs"
+  ],
+  "brand": "Knoll",
+  "images": [
+  "https://cdn.dummyjson.com/products/images/furniture/Knoll%20Saarinen%20Executive%20Conference%20Chair/1.png",
+  "https://cdn.dummyjson.com/products/images/furniture/Knoll%20Saarinen%20Executive%20Conference%20Chair/2.png",
+  "https://cdn.dummyjson.com/products/images/furniture/Knoll%20Saarinen%20Executive%20Conference%20Chair/3.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/furniture/Knoll%20Saarinen%20Executive%20Conference%20Chair/thumbnail.png"
+  },
+  {
+  "id": 15,
+  "title": "Wooden Bathroom Sink With Mirror",
+  "description": "The Wooden Bathroom Sink with Mirror is a unique and stylish addition to your bathroom, featuring a wooden sink countertop and a matching mirror.",
+  "category": "furniture",
+  "price": 799.99,
+  "stock": 95,
+  "tags": [
+  "furniture",
+  "bathroom"
+  ],
+  "brand": "Bath Trends",
+  "images": [
+  "https://cdn.dummyjson.com/products/images/furniture/Wooden%20Bathroom%20Sink%20With%20Mirror/1.png",
+  "https://cdn.dummyjson.com/products/images/furniture/Wooden%20Bathroom%20Sink%20With%20Mirror/2.png",
+  "https://cdn.dummyjson.com/products/images/furniture/Wooden%20Bathroom%20Sink%20With%20Mirror/3.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/furniture/Wooden%20Bathroom%20Sink%20With%20Mirror/thumbnail.png"
+  },
+  {
+  "id": 16,
+  "title": "Apple",
+  "description": "Fresh and crisp apples, perfect for snacking or incorporating into various recipes.",
+  "category": "groceries",
+  "price": 1.99,
+  "stock": 9,
+  "tags": [
+  "fruits"
+  ],
+  "images": [
+  "https://cdn.dummyjson.com/products/images/groceries/Apple/1.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/groceries/Apple/thumbnail.png"
+  },
+  {
+  "id": 17,
+  "title": "Beef Steak",
+  "description": "High-quality beef steak, great for grilling or cooking to your preferred level of doneness.",
+  "category": "groceries",
+  "price": 12.99,
+  "stock": 96,
+  "tags": [
+  "meat"
+  ],
+  "images": [
+  "https://cdn.dummyjson.com/products/images/groceries/Beef%20Steak/1.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/groceries/Beef%20Steak/thumbnail.png"
+  },
+  {
+  "id": 18,
+  "title": "Cat Food",
+  "description": "Nutritious cat food formulated to meet the dietary needs of your feline friend.",
+  "category": "groceries",
+  "price": 8.99,
+  "stock": 13,
+  "tags": [
+  "pet supplies",
+  "cat food"
+  ],
+  "images": [
+  "https://cdn.dummyjson.com/products/images/groceries/Cat%20Food/1.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/groceries/Cat%20Food/thumbnail.png"
+  },
+  {
+  "id": 19,
+  "title": "Chicken Meat",
+  "description": "Fresh and tender chicken meat, suitable for various culinary preparations.",
+  "category": "groceries",
+  "price": 9.99,
+  "stock": 69,
+  "tags": [
+  "meat"
+  ],
+  "images": [
+  "https://cdn.dummyjson.com/products/images/groceries/Chicken%20Meat/1.png",
+  "https://cdn.dummyjson.com/products/images/groceries/Chicken%20Meat/2.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/groceries/Chicken%20Meat/thumbnail.png"
+  },
+  {
+  "id": 20,
+  "title": "Cooking Oil",
+  "description": "Versatile cooking oil suitable for frying, sautéing, and various culinary applications.",
+  "category": "groceries",
+  "price": 4.99,
+  "stock": 22,
+  "tags": [
+  "cooking essentials"
+  ],
+  "images": [
+  "https://cdn.dummyjson.com/products/images/groceries/Cooking%20Oil/1.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/groceries/Cooking%20Oil/thumbnail.png"
+  },
+  {
+  "id": 21,
+  "title": "Cucumber",
+  "description": "Crisp and hydrating cucumbers, ideal for salads, snacks, or as a refreshing side.",
+  "category": "groceries",
+  "price": 1.49,
+  "stock": 22,
+  "tags": [
+  "vegetables"
+  ],
+  "images": [
+  "https://cdn.dummyjson.com/products/images/groceries/Cucumber/1.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/groceries/Cucumber/thumbnail.png"
+  },
+  {
+  "id": 22,
+  "title": "Dog Food",
+  "description": "Specially formulated dog food designed to provide essential nutrients for your canine companion.",
+  "category": "groceries",
+  "price": 10.99,
+  "stock": 40,
+  "tags": [
+  "pet supplies",
+  "dog food"
+  ],
+  "images": [
+  "https://cdn.dummyjson.com/products/images/groceries/Dog%20Food/1.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/groceries/Dog%20Food/thumbnail.png"
+  },
+  {
+  "id": 23,
+  "title": "Eggs",
+  "description": "Fresh eggs, a versatile ingredient for baking, cooking, or breakfast.",
+  "category": "groceries",
+  "price": 2.99,
+  "stock": 10,
+  "tags": [
+  "dairy"
+  ],
+  "images": [
+  "https://cdn.dummyjson.com/products/images/groceries/Eggs/1.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/groceries/Eggs/thumbnail.png"
+  },
+  {
+  "id": 24,
+  "title": "Fish Steak",
+  "description": "Quality fish steak, suitable for grilling, baking, or pan-searing.",
+  "category": "groceries",
+  "price": 14.99,
+  "stock": 99,
+  "tags": [
+  "seafood"
+  ],
+  "images": [
+  "https://cdn.dummyjson.com/products/images/groceries/Fish%20Steak/1.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/groceries/Fish%20Steak/thumbnail.png"
+  },
+  {
+  "id": 25,
+  "title": "Green Bell Pepper",
+  "description": "Fresh and vibrant green bell pepper, perfect for adding color and flavor to your dishes.",
+  "category": "groceries",
+  "price": 1.29,
+  "stock": 89,
+  "tags": [
+  "vegetables"
+  ],
+  "images": [
+  "https://cdn.dummyjson.com/products/images/groceries/Green%20Bell%20Pepper/1.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/groceries/Green%20Bell%20Pepper/thumbnail.png"
+  },
+  {
+  "id": 26,
+  "title": "Green Chili Pepper",
+  "description": "Spicy green chili pepper, ideal for adding heat to your favorite recipes.",
+  "category": "groceries",
+  "price": 0.99,
+  "stock": 8,
+  "tags": [
+  "vegetables"
+  ],
+  "images": [
+  "https://cdn.dummyjson.com/products/images/groceries/Green%20Chili%20Pepper/1.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/groceries/Green%20Chili%20Pepper/thumbnail.png"
+  },
+  {
+  "id": 27,
+  "title": "Honey Jar",
+  "description": "Pure and natural honey in a convenient jar, perfect for sweetening beverages or drizzling over food.",
+  "category": "groceries",
+  "price": 6.99,
+  "stock": 25,
+  "tags": [
+  "condiments"
+  ],
+  "images": [
+  "https://cdn.dummyjson.com/products/images/groceries/Honey%20Jar/1.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/groceries/Honey%20Jar/thumbnail.png"
+  },
+  {
+  "id": 28,
+  "title": "Ice Cream",
+  "description": "Creamy and delicious ice cream, available in various flavors for a delightful treat.",
+  "category": "groceries",
+  "price": 5.49,
+  "stock": 76,
+  "tags": [
+  "desserts"
+  ],
+  "images": [
+  "https://cdn.dummyjson.com/products/images/groceries/Ice%20Cream/1.png",
+  "https://cdn.dummyjson.com/products/images/groceries/Ice%20Cream/2.png",
+  "https://cdn.dummyjson.com/products/images/groceries/Ice%20Cream/3.png",
+  "https://cdn.dummyjson.com/products/images/groceries/Ice%20Cream/4.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/groceries/Ice%20Cream/thumbnail.png"
+  },
+  {
+  "id": 29,
+  "title": "Juice",
+  "description": "Refreshing fruit juice, packed with vitamins and great for staying hydrated.",
+  "category": "groceries",
+  "price": 3.99,
+  "stock": 99,
+  "tags": [
+  "beverages"
+  ],
+  "images": [
+  "https://cdn.dummyjson.com/products/images/groceries/Juice/1.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/groceries/Juice/thumbnail.png"
+  },
+  {
+  "id": 30,
+  "title": "Kiwi",
+  "description": "Nutrient-rich kiwi, perfect for snacking or adding a tropical twist to your dishes.",
+  "category": "groceries",
+  "price": 2.49,
+  "stock": 1,
+  "tags": [
+  "fruits"
+  ],
+  "images": [
+  "https://cdn.dummyjson.com/products/images/groceries/Kiwi/1.png"
+  ],
+  "thumbnail": "https://cdn.dummyjson.com/products/images/groceries/Kiwi/thumbnail.png"
+  }
+]
+
+const productList = document.querySelector('.productList');
+let filteredText = ''; 
+const filterForm = document.getElementById('filterForm');
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+ filteredText = filterForm.filteredText.value;
+ render();
+}
+
+filterForm.addEventListener('submit', handleSubmit);
+
+
+function getProductTagsHtml(tags) {
+
+  let tagsHtml = '';
+  for(const tag of tags) {
+      tagsHtml += `<li>${tag}</li>`;
+  }
+  return tagsHtml;
+}
+
+function render() {
+
+  productList.innerHTML = '';
+for(const product of products) {
+
+  if(product.title.includes(filteredText) !== true) {
+      continue;
+  } 
+
+  productList.innerHTML += `<li class= product-card>
+  <figure>
+  <img src="${product.thumbnail}"></img>
+  </figure>
+  <h4>${product.category}</h4>
+  <h3>${product.title}</h3>
+  <p>${product.description}</p>
+  <div class="product-footer">
+  <strong>${product.price}</strong> | <strong>${product.stock}</strong>
+  </div>
+  <ul class"product-tags">${getProductTagsHtml(product.tags)}</ul>
+  </li>`;
+}
+}
